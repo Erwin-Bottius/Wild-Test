829a00ecf0c7b99c95c0843452b8b617db0965a5
Erwin-Bottius <erwinbottius@gmail.com>
2021-10-11 19:27:32 +0200

    correction ssl option

--- app/db.js
+++ app/db.js
@@ -4,6 +4,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   define: {
     timestamps: false,
   },
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: true,
+  },
 });
 
 module.exports = sequelize;
