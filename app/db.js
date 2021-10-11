a5d1cfc91b93e82f2f4849bf6dad1eda445556be
Erwin-Bottius <erwinbottius@gmail.com>
2021-10-11 19:47:14 +0200

    correction ssl

--- app/db.js
+++ app/db.js
@@ -1,12 +1,17 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  ssl: true,
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   define: {
     timestamps: false,
-  },
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: true,
   },
 });
 
